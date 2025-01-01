d0e6d6016150526a049c42d90a007cc174584666
Blake Ridgway <blake@blakeridgway.dev>
2024-12-31 19:14:52 -0600

    (feat): Seperated database models to different files

--- database.py
+++ database.py
@@ -1,12 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
+migrate = Migrate()
 
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(200), nullable=False)
-    status = db.Column(db.String(50), default="Not Started")
-    month_name = db.Column(db.String(50), nullable=False)
-
-    def __repr__(self):
-        return f'<Task {self.description}>'
+def init_app(app):
+    db.init_app(app)
+    migrate.init_app(app, db)
+    with app.app_context():
+        db.create_all()
---
+++ models/task.py
@@ -0,0 +1,9 @@
+from database import db
+
+class Task(db.Model):
+    __tablename__ = 'tasks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(200), nullable=False)
+    status = db.Column(db.String(50), nullable=False)
+    month_id = db.Column(db.Integer, db.ForeignKey('months.id'), nullable=False)
